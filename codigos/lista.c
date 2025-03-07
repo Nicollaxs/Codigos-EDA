c34740d6a566597e895f8f37cb8f18bdafb35d72
Nicollas Gabriel <nicollason@gmail.com>
2025-03-07 18:30:09 -0300

    codigo

--- codigos/lista.c
+++ codigos/lista.c
@@ -9,12 +9,44 @@ typedef struct Ponto {
 
 Ponto *comeco = NULL;
 
-void add(float x, float y){
+void addComeco(float x, float y){
     Ponto *p = (Ponto*) malloc(sizeof(Ponto)); // Alocação dinamica
     p->x = x;
     p->y = y;
-    p->prox = comeco; // o proximo será null nesse ponto 
+    p->prox = comeco; // o proximo será null nesse ponto ou caso tenha algum item na lista irá apontar para este item
     comeco = p; // Agora "p" é o primeiro item da lista
+}
+
+void addFim(float x, float y){
+
+    Ponto *novo = (Ponto*) malloc(sizeof(Ponto));
+    novo->x = x;
+    novo->y = y;
+    novo->prox = NULL;
+
+    if(comeco == NULL){
+        comeco = novo;
+    } else {
+        Ponto *aux = comeco;
+        while(aux->prox != NULL){
+            aux = aux->prox;
+        }
+        aux->prox = novo;
+    }
+}
+
+void addFimRecursivamente(Ponto *aux, float x, float y){
+
+    if(aux->prox == NULL){
+        Ponto *novo = (Ponto*) malloc(sizeof(Ponto));
+        novo->x = x;
+        novo->y = y;
+        novo->prox = NULL;
+        aux->prox = novo;
+    } else {
+        addFimRecursivamente(aux->prox,x,y);
+    }
+
 }
 
 void imprimir(){
@@ -27,7 +59,7 @@ void imprimir(){
 
 void imprimirRecursivamente(Ponto *p){
     if(p == NULL)
-        printf("\n Acabou");
+        printf("\n Deu certo");
     
     printf("( %.1f, %.1f )\n", p->x,p->y);
     imprimirRecursivamente(p->prox);
@@ -35,9 +67,12 @@ void imprimirRecursivamente(Ponto *p){
 
 int main(){
 
-    add(1,2);
-    add(5,4);
-    add(6,7);
+    addComeco(1,2);
+    addComeco(5,4);
+    addComeco(6,7);
+
+    addFimRecursivamente(comeco,8,9);
+    addFimRecursivamente(comeco,10,11);
 
     imprimirRecursivamente(comeco);
 
