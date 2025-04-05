4d19f2759cfa25b2a74895aba1e87b319c1e944a
Nicollas Gabriel <nicollason@gmail.com>
2025-04-05 07:56:57 -0300

    cod

--- codigos_aula/arvoreBst.c
+++ codigos_aula/arvoreBst.c
@@ -2,77 +2,79 @@
 #include <stdlib.h>
 
 typedef struct STnode* link;
-#define NULLITEM -1
 
-struct STnode{
+struct STnode {
     link l;
     link r;
     int item;
     int n;
 };
 
-link h,z;
+#define NULLITEM -1
 
-link new(link l, link r, int item, int n){
-    link x = malloc(sizeof(*x));;
-    x->l = NULL;
-    x->r = NULL;
+link h, z;
+
+link new(link l, link r, int item, int n) {
+    link x = malloc(sizeof(*x));
+    x->l = l;
+    x->r = r;
     x->item = item;
     x->n = n;
-
     return x;
 }
 
-void STinit(){ 
-    z = new(NULL,NULL,NULLITEM,0); //não entendi esse item NULL
+void STinit() {
+    z = new(NULL, NULL, NULLITEM, 0);
     h = z;
 }
 
-int stcount(){
+int stcount() {
     return h->n;
 }
 
-void sortR(link n){
-    if(n == NULL)
-        return ;   
+void sortR(link n) {
+    if (n == z) return;
     sortR(n->l);
-    printf("%d ", n->item); 
+    printf("%d ", n->item);
     sortR(n->r);
 }
 
-link insertT(link h, int item){
-    if(h == z)
-        return new(z,z,item,1);
-    
-    if(item < h->item)
-       h->l = insertT(h->l, item);
-        //rotação
-    else    
-       h->r = insertT(h->r,item);
-        //rotação
-    h->n = h->l->n + h->r->n + 1; //não entendi
+link insertT(link h, int item) {
+    if (h == z)
+        return new(z, z, item, 1);
 
+    if (item < h->item)
+        h->l = insertT(h->l, item);
+    else
+        h->r = insertT(h->r, item);
+
+    h->n = h->l->n + h->r->n + 1;
     return h;
 }
 
-int search(link h, int item){
-    if(h->item == item)
-        return item;
-
-    if(item < h->item)
-        return search(h->l, item);
-    else    
-        return search(h->r, item); 
+void STinsert(int item) {
+    h = insertT(h, item);
 }
 
+int search(link h, int item) {
+    if (h == z) return NULLITEM;
+    if (h->item == item)
+        return item;
+    if (item < h->item)
+        return search(h->l, item);
+    else
+        return search(h->r, item);
+}
 
+// EXEMPLO DE USO:
+int main() {
+    STinit();
+    STinsert(10);
+    STinsert(5);
+    STinsert(15);
+    STinsert(3);
+    STinsert(7);
 
-
-
-
-
-
-int main(){
-
+    sortR(h); // Vai imprimir: 3 5 7 10 15
     return 0;
 }
